f2aad856f0065c000e8ea5d1983ad108e1158bc0
Jefferson Murat <72477374+JrMurat12@users.noreply.github.com>
2022-11-21 15:24:05 -0300

    Add files via upload
    POO - Alura

---
+++ PythonAlura/POO/Conta.py
@@ -0,0 +1,21 @@
+class Conta:
+    
+    def __init__(self, numero, titular, saldo, limite):
+        print ("Contruindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+        
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
