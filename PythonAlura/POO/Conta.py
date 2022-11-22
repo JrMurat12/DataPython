78408ea2b2e9e7d42f1cd3f679df1c187c21b965
Jefferson Murat <72477374+JrMurat12@users.noreply.github.com>
2022-11-21 23:02:55 -0300

    Update Conta.py

--- PythonAlura/POO/Conta.py
+++ PythonAlura/POO/Conta.py
@@ -19,3 +19,15 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+        
+    def get_saldo(self):
+        return self.__saldo
+    
+    def get_titular(self):
+        return self.__titular
+        
+    def get_limite(self):
+        return self.__limite
+    
+    def set_limite(self, limite):
+        self.__limite = limite
