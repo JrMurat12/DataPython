96d3cb5929b0a6c5036a322122d966c2c660546e
JrMurat12 <jr-murat@hotmail.com>
2022-11-23 17:49:31 -0300

    POO2 Atualizado

--- PythonAlura/POO2/Modelo.py
+++ PythonAlura/POO2/Modelo.py
@@ -1,46 +1,34 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+    
+    @property
+    def likes(self):
+        return self._likes
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+    
+    def dar_like(self):
+        self._likes += 1
+
+class Filme (Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
-    
-    @property
-    def likes(self):
-        return self.__likes
-    
-    @property
-    def nome(self):
-        return self.__nome
-    
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-    
-    def dar_like(self):
-        self.__likes += 1
         
-class Serie:
+class Serie (Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
-    
-    @property
-    def likes(self):
-        return self.__likes
-    
-    @property
-    def nome(self):
-        return self.__nome
-    
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-    
-    def dar_like(self):
-        self.likes += 1
+
         
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
