952dcf70e0c638338c1ec96099afa66d90fe3489
jefferson.murat <jr-murat@hotmail.com>
2022-11-22 22:11:19 -0300

    Finalizando Curso POO

--- PythonAlura/POO/Conta.py
+++ PythonAlura/POO/Conta.py
@@ -1,7 +1,7 @@
 class Conta:
-    
+
     def __init__(self, numero, titular, saldo, limite):
-        print ("Contruindo objeto ... {}".format(self))
+        print("Construindo objeto ... {}".format(self))
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
@@ -13,23 +13,40 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
-        
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
+
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
-        
-    def get_saldo(self):
+
+    @property
+    def saldo(self):
         return self.__saldo
-    
+
+    @property
     def get_titular(self):
         return self.__titular
-        
-    def get_limite(self):
+
+    @property
+    def limite(self):
         return self.__limite
-    
-    def set_limite(self, limite):
+
+    @limite.setter
+    def limite(self, limite):
         self.__limite = limite
-        
-        
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
