246e2aad0f0190316fbe7c882a3c4a0d7b4fd512
JrMurat12 <jr-murat@hotmail.com>
2022-11-24 18:06:02 -0300

    POO2 Atuliazado Novo

--- PythonAlura/POO2/Modelo.py
+++ PythonAlura/POO2/Modelo.py
@@ -18,26 +18,36 @@ class Programa:
     
     def dar_like(self):
         self._likes += 1
+        
+    
 
 class Filme (Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
         
+    def imprime(self):
+        print(f'Nome: {self._nome} - Ano: {self.ano} - Duração: {self.duracao} Min - Likes: {self._likes}')
+        
 class Serie (Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+        
+    def imprime(self):
+        print(f'Nome: {self._nome} - Ano: {self.ano} - Temporadas: {self.temporadas} - Likes: {self._likes}')
 
         
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Temporadas: {vingadores.duracao} - Likes: {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
 
-atlanta.dar_like()
+filme_e_series = [vingadores, atlanta]
+
+for programa in filme_e_series:
+    programa.imprime()
+    
