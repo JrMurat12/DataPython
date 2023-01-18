14d8d9952bd3bac6d54971f539a656b24d5a8c21
jefferson.murat <jr-murat@hotmail.com>
2023-01-17 22:06:43 -0300

    TDD Python 17-01 v3

--- PythonAlura/TDD_Python/tests/test_bytebank.py
+++ PythonAlura/TDD_Python/tests/test_bytebank.py
@@ -3,9 +3,9 @@ import pytest
 from pytest import mark
 
 class TestClass:
-    def test_quando_idade_recebe_13_03_2000_deve_retornar_23(self):
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
         entrada = '13/03/2000' # Given-Contexto
-        esperado = 23
+        esperado = 22
 
         funcionario_teste = Funcionario('Teste', entrada, 1111)
         resultado = funcionario_teste.idade() # When-ação
@@ -21,7 +21,6 @@ class TestClass:
 
         assert resultado == esperado
 
-    @mark.skip
     def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
         entrada_salario = 100000 #given
         entrada_nome = 'Paulo Bragança'
@@ -32,25 +31,26 @@ class TestClass:
         resultado = funconario_teste.salario
 
         assert resultado == esperado  # then
-    
-    #@mark.calcular_bonus    
+
+    @mark.calcular_bonus
     def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
-        entrada = 1000 #Given
+        entrada = 1000  # given
         esperado = 100
-        
-        Funcionario_Teste = Funcionario('Teste', '11/11/2000', entrada)
-        resultado = Funcionario_Teste.calcular_bonus() # When-Ação
-        
-        assert resultado == esperado # Then-Desfecho
-    
-    #@mark.calcular_bonus      
-    def test_quando_calcular_bonus_recebe_1000000_deve_retornar_exception(self):
+
+        funconario_teste = Funcionario('teste', '11/11/2000', entrada)
+        resultado = funconario_teste.calcular_bonus() # when
+
+        assert resultado == esperado  # then
+
+    @mark.calcular_bonus
+    def test_quando_calcular_bonus_recebe_100000000_deve_retornar_exception(self):
         with pytest.raises(Exception):
-            entrada = 1000000 #Given
-        
-            Funcionario_Teste = Funcionario('Teste', '11/11/2000', entrada)
-            resultado = Funcionario_Teste.calcular_bonus() # When-Ação
-        
-            assert resultado # Then-Desfecho
+            entrada = 100000000  # given
+
+            funconario_teste = Funcionario('teste', '11/11/2000', entrada)
+            resultado = funconario_teste.calcular_bonus()  # when
+
+            assert resultado  # then
+
         
         
