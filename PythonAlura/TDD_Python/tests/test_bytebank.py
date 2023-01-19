b17df140ca5bc15cbdc41dfc42a5d56d338e88af
JrMurat12 <jr-murat@hotmail.com>
2023-01-19 11:08:04 -0300

    TDD Python 19-01

--- PythonAlura/TDD_Python/tests/test_bytebank.py
+++ PythonAlura/TDD_Python/tests/test_bytebank.py
@@ -3,9 +3,9 @@ import pytest
 from pytest import mark
 
 class TestClass:
-    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_23(self):
         entrada = '13/03/2000' # Given-Contexto
-        esperado = 22
+        esperado = 23
 
         funcionario_teste = Funcionario('Teste', entrada, 1111)
         resultado = funcionario_teste.idade() # When-ação
